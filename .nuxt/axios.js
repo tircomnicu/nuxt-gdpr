c8106b4b967018b9fae15bec5fa5c9f390b0ab7c
Vlad Tircomnicu <tircomnicu@gmail.com>
2019-12-25 21:34:49 +0200

    add axios changes

--- .nuxt/axios.js
+++ .nuxt/axios.js
@@ -106,8 +106,8 @@ const setupProgress = (axios, ctx) => {
 export default (ctx, inject) => {
   // baseURL
   const baseURL = process.browser
-      ? 'http://localhost:3002/'
-      : (process.env._AXIOS_BASE_URL_ || 'http://localhost:3002/')
+      ? 'http://localhost:3001/'
+      : (process.env._AXIOS_BASE_URL_ || 'http://localhost:3001/')
 
   // Create fresh objects for all default header scopes
   // Axios creates only one which is shared across SSR requests!
